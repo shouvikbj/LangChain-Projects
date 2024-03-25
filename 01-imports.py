33698dc2b7ab0a56e938eb6441eeb8928a3705bb
Shouvik Bajpayee <shouvikbajpayee@Shouviks-MacBook-Air.local>
2024-03-25 14:03:32 +0530

    first commit

---
+++ 01-imports.py
@@ -0,0 +1,10 @@
+# Imports in Terminal
+
+# Installing LangChain & LangChain Experimental
+# python3 -m pip install langchain langchain_experimental
+
+# Installing HuggingFace Hub
+# python3 -m pip install huggingface_hub
+
+# Installing TikToken and Chroma
+# python3 -m pip install tiktoken chromadb
---
+++ README.md
@@ -0,0 +1,5 @@
+# LangChain Projects (Python)
+This repository contains projects using LangChain
+
+## Author
+Shouvik Bajpayee
